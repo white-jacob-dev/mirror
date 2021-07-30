2ea00cf4c89c10ec7cd2e930a35dfee986269aed
Jacob White <white.jacob.dev@gmail.com>
2021-07-30 00:03:06 -0400

    Added neomorphismic styling

--- scraper.js
+++ scraper.js
@@ -1,42 +1,43 @@
-const express = require('express');
-const puppeteer = require('puppeteer');
-const path = require('path');
+const express = require("express");
+const puppeteer = require("puppeteer");
+const path = require("path");
 const router = express.Router();
 
-const app = express()
-const port = 3000
-
-
+const app = express();
+const port = 3000;
 
 async function scrapeProduct(url) {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
 
-    const [el] = await page.$x('//*[@id="main"]/div/div/div/section[1]/div[2]/div[2]/div[3]/a/div[1]/div[1]');
-    const txt = await el.getProperty('textContent');
-    const rawTxt = await txt.jsonValue();
+  const [el] = await page.$x(
+    '//*[@id="main"]/div/div/div/section[1]/div[2]/div[2]/div[3]/a/div[1]/div[1]'
+  );
+  const txt = await el.getProperty("textContent");
+  const rawTxt = await txt.jsonValue();
 
-    browser.close();
+  browser.close();
 
-    console.log({rawTxt})
+  console.log({ rawTxt });
 
-    return {rawTxt}
+  return { rawTxt };
 }
 
-
-router.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/index.html'));
+router.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-router.get('/data', async function(req, res) {
-    let data = await scrapeProduct('https://www.fcbarcelona.com/en/football/first-team/schedule');
-    res.send(data)
-  })
+router.get("/data", async function (req, res) {
+  let data = await scrapeProduct(
+    "https://www.fcbarcelona.com/en/football/first-team/schedule"
+  );
+  res.send(data);
+});
 
-  app.use('/', router);
+app.use(express.static(__dirname + '/design'));
+app.use("/", router);
 
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-  })
-
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
