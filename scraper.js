0d29635b167cccf40734714ac2c68062c849f4ab
Jacob White <white.jacob.dev@gmail.com>
2021-07-31 16:35:51 -0400

    Added game time, conversion to EST, and AM/PM formatting

--- scraper.js
+++ scraper.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const app = express();
 const port = 3000;
 
-//Get date and opponent of next game
+//Get date, opponent, and time of next game
 async function scrapeGameData(url) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
@@ -19,6 +19,13 @@ async function scrapeGameData(url) {
   const nextGameTxt = await nextGame.getProperty("textContent");
   const nextGameRawTxt = await nextGameTxt.jsonValue();
 
+  //Get time of the next game
+  const [nextGameTime] = await page.$x(
+    '//*[@id="main"]/div/div/div/section[1]/div[2]/div[2]/div[3]/a/div[1]/div[2]'
+  );
+  const nextGameTimeTxt = await nextGameTime.getProperty("textContent");
+  const nextGameTimeRawTxt = await nextGameTimeTxt.jsonValue();
+
   //Get the next opponent
   const [nextOpponent] = await page.$x(
     '//*[@id="main"]/div/div/div/section[1]/div[3]/div/div[2]/div[2]/a/div[1]/div[1]'
@@ -27,29 +34,27 @@ async function scrapeGameData(url) {
   const nextOpponentRawTxt = await nextOpponentTxt.jsonValue();
 
   browser.close();
-  return { nextGameRawTxt, nextOpponentRawTxt};
+  return { nextGameRawTxt, nextGameTimeRawTxt, nextOpponentRawTxt, };
 }
 
-//Get the next opponent's logo
-async function scrapeLogo(url) {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(url);
+// -------------WORK IN PROGRESS------------------
+// //Get the next opponent's logo
+// async function scrapeLogo(url) {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+//   await page.goto(url);
 
-  const [nextOpponentLogo] = await page.$x(
-    '//*[@id="main"]/div/div/div/section[1]/div[3]/div/div[2]/div[2]/a/div[1]/div[2]/span[1]/i'
-  );
-  await page.evaluate(() => document.body.style.background = 'transparent');
-  await nextOpponentLogo.screenshot({path:"./design/logo.png", omitBackground: true});
+//   const [nextOpponentLogo] = await page.$x(
+//     '//*[@id="main"]/div/div/div/section[1]/div[3]/div/div[2]/div[2]/a/div[1]/div[2]/span[1]/i'
+//   );
+//   await nextOpponentLogo.screenshot({path:"./design/logo.png", omitBackground: true});
 
-  browser.close();
-  return;
-}
-
-
+//   browser.close();
+//   return;
+// }
 
 router.get("/", async function (req, res) {
-  await scrapeLogo("https://www.fcbarcelona.com/en/football/first-team/schedule")
+  // await scrapeLogo("https://www.fcbarcelona.com/en/football/first-team/schedule")
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 
